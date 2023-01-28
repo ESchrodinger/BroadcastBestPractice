85b809c46b437397a52c9aae5c5a9a1a83428b1c
zjiang.li <1002029005@qq.com>
2023-01-28 11:15:03 +0800

    公司能用的版本，并且加上了记住密码的功能

--- app/src/main/java/com/srcgzjiang/broadcastbestpractice/BaseActivity.java
+++ app/src/main/java/com/srcgzjiang/broadcastbestpractice/BaseActivity.java
@@ -46,23 +46,23 @@ public class BaseActivity extends AppCompatActivity {
         }
     }
 
-    class ForceOfflineReceiver extends BroadcastReceiver{
-
-        @Override
-        public void onReceive(final Context context, Intent intent){
-            AlertDialog.Builder builder = new AlertDialog.Builder(context);
-            builder.setTitle("Warning");
-            builder.setMessage("You are forced to be offline. Please try to login again");
-            builder.setCancelable(false);
-            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialogInterface, int i) {
-                    ActivityCollector.finishAll();
-                    Intent intent = new Intent(context, LoginActivity.class);
-                    context.startActivity(intent);
-                }
-            });
-            builder.show();
-        }
-    }
+//    class ForceOfflineReceiver extends BroadcastReceiver{
+//
+//        @Override
+//        public void onReceive(final Context context, Intent intent){
+//            AlertDialog.Builder builder = new AlertDialog.Builder(context);
+//            builder.setTitle("Warning");
+//            builder.setMessage("You are forced to be offline. Please try to login again");
+//            builder.setCancelable(false);
+//            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+//                @Override
+//                public void onClick(DialogInterface dialogInterface, int i) {
+//                    ActivityCollector.finishAll();
+//                    Intent intent = new Intent(context, LoginActivity.class);
+//                    context.startActivity(intent);
+//                }
+//            });
+//            builder.show();
+//        }
+//    }
 }
